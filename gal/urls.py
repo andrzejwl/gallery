bb7e3a11010b791407ce9dadfabd7c9aa401dc2e
andrzejwl <andrzej.wlodarczyk@husarnet.com>
2020-06-12 19:59:30 +0200

    removing old urls

--- gal/urls.py
+++ gal/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    path('panel', views.panel, name='panel'),
     path('logout', views.logout, name='logout'),
     path('upload', views.upload, name='upload'),
     path('create_category', views.create_category, name='create_category'),
